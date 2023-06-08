225ff32e7848953b26b5e1a2930ff0ac4c3dbf38
adhoc0909 <adhoc0909@khu.ac.kr>
2023-06-08 21:50:17 +0900

    version3: bug fixed

--- models/training.py
+++ models/training.py
@@ -167,6 +167,8 @@ class Synthesized_model():
                 print("D: {}".format(self.losses['D'][-1]))
                 print("GP: {}".format(self.losses['GP'][-1]))
                 print("Gradient norm: {}".format(self.losses['gradient_norm'][-1]))
+                print("self_rec: {}".format(self.losses['self_rec'][-1]))
+                print("id_loss: {}".format(self.losses['id_loss'][-1]))
                 if self.num_steps > self.critic_iter:
                     print("G: {}".format(self.losses['G'][-1]))
 
